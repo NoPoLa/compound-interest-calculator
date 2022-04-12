d6178f29f97b56d7da3366b94c24ff8fc024bb44
NoPoLa <41204899+NoPoLa@users.noreply.github.com>
2022-04-12 22:52:28 +0200

    chore: add font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        serif: ["Playfair Display", "serif"],
+      },
+    },
   },
   plugins: [require("@tailwindcss/forms")],
 };
